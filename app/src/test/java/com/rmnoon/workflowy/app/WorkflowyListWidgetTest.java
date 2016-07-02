e785853e818e5d3f9d0b10108faefbd2653ab2c5
Ryan Noon <rmnoon@cs.stanford.edu>
2016-07-01 17:01:41 -0700

    soft keyboard pops up when adding or editing an item, added screenshots and widget preview, fixed strings with proper capitalization of WorkFlowy

--- app/src/test/java/com/rmnoon/workflowy/app/WorkflowyListWidgetTest.java
+++ app/src/test/java/com/rmnoon/workflowy/app/WorkflowyListWidgetTest.java
@@ -28,27 +28,31 @@ public class WorkflowyListWidgetTest {
         WFTestUtil.clearLists(client);
 
         WFList todo = client.createRootList(0, "todo", "my todo list");
-        WFList buyMilk = client.createList(todo, 0, "buy blue milk", null);
-        WFList buyCereal = client.createList(todo, 1, "buy cereal", null);
-        WFList powerConverts = client.createList(todo, 2, "pick up power converters", "at toshe station");
+        WFList blueMilk = client.createList(todo, 0, "buy blue milk", null);
+        WFList protocolDroid = client.createList(todo, 1, "find a new protocol droid", "must be able to speak Bocce");
+        client.completeList(protocolDroid, true);
+        client.createList(todo, 2, "pick up power converters", "at toshe station");
+        client.createList(todo, 3, "study for the Academy entrance test", null);
+        client.createList(todo, 4, "investigate womp rat infestation", null);
 
-        WFList ideas = client.createRootList(1, "ideas", "ideas that come to me");
-        WFList supremeExecutivePower = client.createList(ideas, 0, "supreme executive power", "derives from a mandate of the masses");
-        WFList gotoSpace = client.createList(ideas, 1, "go to space", null);
-        client.completeList(gotoSpace, true);
-        WFList holyRoman = client.createList(ideas, 2, "the holy roman empire", "was neither holy nor roman nor an empire");
-        WFList ifGod = client.createList(ideas, 3, "if god did not exist", "it would be necessary to invent him");
+        WFList ideas = client.createRootList(1, "ideas", "my greatest ideas");
+        WFList podracing = client.createList(ideas, 0, "podracing: is it still a thing?", null);
+        WFList mosEisley = client.createList(ideas, 1, "mos eisley is a wretched hive of scum and villainy but its really quite livable and the bars are great.", null);
+        WFList sandPeople = client.createList(ideas, 2, "why do sand people always walk single file?", null);
+        client.completeList(sandPeople, true);
+        WFList biggs = client.createList(ideas, 3, "biggs darklighter", "i wonder how he's doing these days");
 
         assertEquals(client.getRootLists().size(), 2);
 
         assertEquals(client.getRootLists().get(0), todo);
-        assertEquals(todo.getChild(0), buyMilk);
-        assertEquals(todo.getChild(1), buyCereal);
+        assertEquals(todo.getChild(0), blueMilk);
+        assertEquals(todo.getChild(1), protocolDroid);
+        assertTrue(protocolDroid.isComplete());
 
         assertEquals(client.getRootLists().get(1), ideas);
-        assertEquals(ideas.getChild(0), supremeExecutivePower);
-        assertEquals(ideas.getChild(1), gotoSpace);
-        assertTrue(gotoSpace.isComplete());
+        assertEquals(ideas.getChild(0), podracing);
+        assertEquals(ideas.getChild(1), mosEisley);
+        assertTrue(sandPeople.isComplete());
     }
 
 }
